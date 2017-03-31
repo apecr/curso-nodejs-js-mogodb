901b9beb4a5bdb546fa72eea69db29432b6a71b9
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2017-03-31 11:19:07 +0200

    Incluimos la funcion async.series
    Incluimos la funcion async.series. Aun queda un trabajo de entenderla bien

--- async/asincrono_async.js
+++ async/asincrono_async.js
@@ -14,9 +14,27 @@ function escribeTrasDosSegundos(texto, callback) {
 
 // bucle asincrono en serie
 
-var lista = [1,2,'tres',4,5];
+var lista = [1, 2, 'tres', 4, 5];
 
 
 async.eachSeries(lista, escribeTrasDosSegundos, function () {
     console.log('He terminado');
+});
+
+async.series({
+    one: function (callback) {
+        setTimeout(function () {
+            callback(null, 1);
+            console.log("one");
+        }, 200);
+    },
+    two: function (callback) {
+        setTimeout(function () {
+            callback(null, 2);
+            console.log("two");
+        }, 100);
+    }
+}, function (err, results) {
+    // results is now equal to: {one: 1, two: 2}
+    console.log("Results", results);
 });
