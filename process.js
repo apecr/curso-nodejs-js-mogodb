7d3f8635be9b618ce16a8ed874b9646f46a1ba51
apecr <albertoeyo@outlook.es>
2017-04-06 00:37:13 +0200

    Process

--- process.js
+++ process.js
@@ -2,7 +2,13 @@
 
 //informacion del proceso
 var info = {
-    pid: process.pid
+    pid: process.pid,
+    version: process.version,
+    platform: process.platform,
+    title: process.title,
+    argumentos: process.argv,
+    execPath: process.execPath,
+    folder: process.cwd()
 };
 
 module.exports = {
