11a6b1db49060297fa85defde708c3bb00bfbace
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2017-04-05 09:01:08 +0200

    Complicando los prototipos con herencia
    En el ejemplo podemos ver como un agente hereda de una persona

--- prototipos.js
+++ prototipos.js
@@ -4,6 +4,16 @@ function Persona(name){
     this.name = name;
 }
 
+function Agente(name){
+    Persona.call(this, name);
+    // esto ejecuta el constructor de Persona con el this de Agente
+    // definiendo en el this de Agente una propiedad name
+    // y asignandole el parametro recibido
+}
+
+// le asignamos con prototipo una persona
+Agente.prototype = new Persona("");
+
 Persona.prototype.greet = function(){
     var output = "Hola me llamo " + this.name;
     console.log(output);
@@ -11,5 +21,6 @@ Persona.prototype.greet = function(){
 }
 
 module.exports = {
-    Persona: Persona
+    Persona: Persona,
+    Agente: Agente
 }
--- test/prototipos-test.js
+++ test/prototipos-test.js
@@ -1,25 +1,32 @@
 "use strict";
 
-const describe = require("mocha").describe;
-const it = require("mocha").it;
 const assert = require("assert");
 const prototipos = require("../prototipos.js");
 
 describe("Using the prototipos.js file", () => {
-    const name = "Neo";
+    const namePerson = "Neo";
+    const nameAgent = "Smith";
     it("Should return OK the Persona constructor", () => {
         //Arrange
         //Act
-        var persona = new prototipos.Persona(name);
+        var persona = new prototipos.Persona(namePerson);
         //Assert
         assert.ok(persona, "Created the person object");
     });
     it("Should wave Neo", () => {
         //Arrange
-        var persona = new prototipos.Persona(name);
+        var persona = new prototipos.Persona(namePerson);
         //Act
         const greet = persona.greet();
         //Assert
-        assert.ok(greet, "Hola me llamo " + name, "Created the person object");
+        assert.ok(greet, "Hola me llamo " + namePerson, "Created the person object");
+    });
+    it("Should works the Agent function", () => {
+        //Arrange
+        var agent = new prototipos.Agente(nameAgent);
+        //Act
+        const greet = agent.greet();
+        //Assert
+        assert.ok(greet, "Hola me llamo " + nameAgent, "Created the person object");
     });
 });
