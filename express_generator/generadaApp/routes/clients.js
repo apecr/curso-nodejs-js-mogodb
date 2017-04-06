5540fb3dec38b23edcb587ceea17aa8e3b0b2f0b
Alberto Eyo <albertoeyo@outlook.es>
2017-04-06 21:40:02 +0200

    Fin modulo 51 paso de parametros
    Fin del modulo 51, hemos visto los pasos de parametros posibles

---
+++ express_generator/generadaApp/routes/clients.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var express = require("express");
+var router = express.Router();
+
+router.get("/", (req, res, next) => {
+    res.send("respuesta a clients");
+});
+router.get("/:id", (req, res, next) => {
+    console.log("req.query " + req.query.name);
+    console.log("Hemos recibido " + req.params.id);
+    res.send("recibido parametro " + req.params.id);
+});
+
+router.post("/", function (req, res, next) {
+    console.log("req.body " + JSON.stringify(req.body));
+    res.json({recibido: req.body.num});
+});
+
+
+module.exports = router;
+
