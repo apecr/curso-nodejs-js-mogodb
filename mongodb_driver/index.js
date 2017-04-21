2724d9f283bbb04f82d8fcd8e07c7f1045deadff
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2017-04-21 08:52:41 +0200

    Ejercicio de MongoDB con promesas
    Ejercicio MongoDB hecho todo con promesas

---
+++ mongodb_driver/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var MongoClient = require('mongodb').MongoClient;
+const URL_BBDD = 'mongodb://localhost:27017/cursonode';
+const TABLE_AGENTS = 'agents';
+
+var onError = function (error) {
+  console.log(error);
+  return process.exit();
+}
+
+var onResponseMongo = function (docs, db){
+  console.log(docs);
+  db.close();
+}
+
+var onConnect =  function (db) {
+  return db.collection(TABLE_AGENTS).find().toArray().then(function (docs) {
+    return onResponseMongo(docs, db);
+  });
+}
+
+
+MongoClient.connect(URL_BBDD).then(onConnect).catch(onError);
