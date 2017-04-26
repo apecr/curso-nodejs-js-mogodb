d16fdfd9410abc1e7e7988f7ca15f86be3da4064
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2017-04-26 09:21:33 +0200

    Fin del curso incluyendo el material

--- express_generator/generadaApp/models/Agent.js
+++ express_generator/generadaApp/models/Agent.js
@@ -7,8 +7,17 @@ var agentSchema = mongoose.Schema({
   age: Number
 });
 
+agentSchema.statics.list = function (filter, limit, skip, fields, sort, callback) {
+  var query = Agent.find(filter);
+  query.limit(limit);
+  query.skip(skip);
+  query.select(fields);
+  query.sort(sort);
+  query.exec(callback);
+}
+
 // Crear
-mongoose.model('Agent', agentSchema);
+var Agent = mongoose.model('Agent', agentSchema);
 
 // Recuperar
 // mongoose.model('Agent');
--- express_generator/generadaApp/routes/apiv1/agents.js
+++ express_generator/generadaApp/routes/apiv1/agents.js
@@ -5,48 +5,65 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Agent = mongoose.model('Agent');
 
-function returnAgentAF(response, agent){
+function returnAgentAF(response, agent) {
   return response.json({ok: true, agent: agent});
 }
 
-function executeResponse(response){
-  return function returnAgent(agent){
+function executeResponse(response) {
+  return function returnAgent(agent) {
     return response.json({ok: true, agent: agent});
   }
 }
 
-function nextError(next, error){
+function nextError(next, error) {
   next(error);
 }
 
-// recuperar lista de agentes
-router.get('/', (req, res, next) => {
-  Agent.find().exec((error, listAgents) => {
+var callbackList = (res, next) => {
+  return (error, listAgents) => {
     if (error) {
       next(error);
       return;
     }
     res.json({ok: true, list: listAgents});
-  });
+  }
+}
+
+// recuperar lista de agentes
+router.get('/', (req, res, next) => {
+  var name = req.query.name;
+  var age = req.query.age;
+  var limit = parseInt(req.query.limit) || null;
+  var skip = parseInt(req.query.skip) || null;
+  var fields = req.query.fields || null;
+  var sort = req.query.sort;
+  var filter = {};
+  if (name){
+    filter.name = name;
+  }
+  if (typeof age !== 'undefined'){
+    filter.age = age;
+  }
+  Agent.list(filter, limit, skip, fields, sort, callbackList(res, next));
 });
 
 // Actualizar un agente
-router.put('/:id', (request, response, next)  => {
+router.put('/:id', (request, response, next) => {
   var promiseUpdate = Agent.update({_id: request.params.id}, request.body);
   console.log(request.params.id);
   promiseUpdate
     .then(executeResponse(response))
-    .catch( error => nextError(next, error));
+    .catch(error => nextError(next, error));
 });
 
 // Crear un agente
 router.post('/', (request, response, next) => {
   console.log(request.body);
-  var  agent = new Agent(request.body);
+  var agent = new Agent(request.body);
   var promiseSave = agent.save();
   promiseSave
     .then((agent) => returnAgentAF(response, agent))
-    .catch( error => nextError(next, error));
+    .catch(error => nextError(next, error));
 });
 // Borrar un agente
 router.delete('/:id', (request, response, next) => {
